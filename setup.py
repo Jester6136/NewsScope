3a9db52b0660686a9f04a79ad722768968bd5b7e
bags <bags@aiacademy.edu.vn>
2024-03-13 13:35:32 +0700

    init

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+
+setup(name = "multimodal-ner",
+    version = "1.0",
+    author = "sonba",
+    author_email = "bagiangson2001@gmail.com",
+    packages=find_packages()
+) 
